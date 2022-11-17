5b7e1df79f7abec4d0a7a9e072f425cf7974ae03
민석 <80371412+MinSeok0123@users.noreply.github.com>
2022-11-17 20:13:31 +0900

    img

--- gnghouse/src/components/Body.jsx
+++ gnghouse/src/components/Body.jsx
@@ -1,10 +1,12 @@
 import styles from "../../styles/Body.module.css";
 import Page1 from "./Page1";
+import Page2 from "./Page2";
 
 export default function Body () {
     return(
         <div>
            <Page1/>
+           <Page2/>
         </div>
     );
 }
---
+++ gnghouse/src/components/Page2.jsx
@@ -0,0 +1,9 @@
+import styles from "../../styles/Page1.module.css";
+
+export default function Page2() {
+  return (
+    <div>
+      <img className={styles.background1} src="/img/background2.png"></img>
+    </div>
+  );
+}
